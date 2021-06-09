e8f430bd888d7402305ebb292d13ab467aadd8e3
nolmegamelab <nolmegamelab@gmail.com>
2021-06-09 23:50:49 +0900

    소스만 추가.

---
+++ RealPlay/Source/RealPlay/RealPlay.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RealPlay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RealPlay, "RealPlay" );
+
+DEFINE_LOG_CATEGORY(LogRealPlay)
+ 
